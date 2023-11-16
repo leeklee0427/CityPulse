9ce5454cd619ba778b704aa8c3c00b4eab86ad39
leeklee0427 <billlee0427@126.com>
2023-11-16 03:31:08 -0500

    Added crawlers

---
+++ crawlers/cost_of_living.py
@@ -0,0 +1,57 @@
+"""
+File: cost_of_living.py
+Author: Yanting HU (yantingh), Daochen LI (daochenl)
+Description: Retrieve city and cost of living (in percentage compared to national average) from Salary.com
+"""
+
+import csv
+from bs4 import BeautifulSoup
+import requests
+
+def write_csv(filename, data):
+    """
+    Write data to a CSV file with a specific format.
+    """
+    with open(filename, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['City', 'State', 'Cost of Living Percentage']) # write the header
+        writer.writerows(data) # write all rows in the list data
+
+major_cities = {'tampa':'fl', 'orlando':'fl', 'austin':'tx', 'san-antonio':'tx', 'charlotte':'nc', 
+                'raleigh':'nc', 'columbus':'oh', 'boston':'ma', 'portland':'or', 'kansas-city':'mo', 
+                'pittsburgh':'pa', 'sacramento':'ca', 'san-diego':'ca', 'dallas':'tx', 'tucson':'az',
+                'seattle':'wa', 'minneapolis':'mn', 'denver':'co', 'atlanta':'ga', 'salt-lake-city':'ut',
+                'honolulu':'hi', 'miami':'fl', 'albuquerque':'nm', 'san-jose':'ca', 'philadelphia':'pa',
+                'phoenix':'az', 'las-vegas':'nv', 'houston':'tx', 'chicago':'il', 'washington':'dc', 
+                'san-francisco':'ca', 'new-york':'ny', 'los-angeles':'ca'}
+
+# URL template with placeholders for city and state
+template = 'https://www.salary.com/research/cost-of-living/{}-{}'
+
+city_state_cost_of_living = []
+
+# Loop through all city-state pairs
+for city, state in major_cities.items():
+
+    url = template.format(city, state)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        percent_span = soup.find('span', class_='percent-color')
+        
+        if percent_span:
+            percent_value = percent_span.get_text(strip=True).split()[0].rstrip('%') # Percentage symbol removed
+            
+            # Add the city, state, and percent value to the list
+            city_state_cost_of_living.append([city.title(), state.upper(), percent_value])
+
+        else:
+            print(f'Percent value not found for {city.title()}, {state.upper()}.')
+    else:
+        print(f'Request failed for {city.title()}, {state.upper()}, Status Code:', response.status_code)
+
+# Write clean data to csv
+filename = './data/clean/cost_of_living.csv'
+print("Writing to " + filename)
+write_csv(filename, city_state_cost_of_living)
---
+++ crawlers/recreation.py
@@ -0,0 +1,73 @@
+"""
+File: recreation.py
+Author: Yanting HU (yantingh), Daochen LI (daochenl)
+Description: Retrieve city and recreation total score data from WalletHub.com
+"""
+
+import csv
+from bs4 import BeautifulSoup
+import requests
+
+
+def write_csv(filename, data):
+    """
+    Write data to a CSV file with a specific format.
+    """
+    with open(filename, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['City', 'State', 'Recreation Total Score']) # write the header
+        writer.writerows(data) # write all rows in the list data
+
+cities_states = {'tampa':'fl', 'orlando':'fl', 'austin':'tx', 'san-antonio':'tx', 'charlotte':'nc', 
+                'raleigh':'nc', 'columbus':'oh', 'boston':'ma', 'portland':'or', 'kansas-city':'mo', 
+                'pittsburgh':'pa', 'sacramento':'ca', 'san-diego':'ca', 'dallas':'tx', 'tucson':'az',
+                'seattle':'wa', 'minneapolis':'mn', 'denver':'co', 'atlanta':'ga', 'salt-lake-city':'ut',
+                'honolulu':'hi', 'miami':'fl', 'albuquerque':'nm', 'san-jose':'ca', 'philadelphia':'pa',
+                'phoenix':'az', 'las-vegas':'nv', 'houston':'tx', 'chicago':'il', 'washington':'dc', 
+                'san-francisco':'ca', 'new-york':'ny', 'los-angeles':'ca'}
+
+cities = []
+states = []
+scores = []
+
+city_state_recreation = []
+
+url = 'https://wallethub.com/edu/best-worst-cities-for-recreation/5144'
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("HTTP request successful.")
+    soup = BeautifulSoup(response.content, 'html.parser')
+    rows = soup.find_all('tr')
+
+    # Loop through each row and extract the city, state, and corresponding score
+    for row in rows:
+        data = row.find_all('td')
+        if data: # Check if there is data
+            city_state = data[1].get_text().strip()
+            score = data[2].get_text().strip()
+
+            # Split city and state
+            city, state = city_state.split(', ')
+
+            # Add the data to the respective lists
+            cities.append(city)
+            states.append(state)
+            scores.append(score)
+
+    cities_states_ = {c.replace("-", " ").lower(): s for c, s in cities_states.items()}
+    filtered_cities = [c for c in cities if c.lower() in cities_states_.keys()]
+
+    for city in filtered_cities:
+        i = cities.index(city)
+        state = states[i]
+        score = scores[i]
+        city_state_recreation.append([city, state.upper(), score])
+    
+    # Write the result to a CSV file
+    filename = './data/clean/recreation.csv'
+    print("Writing to " + filename)
+    write_csv(filename, city_state_recreation)
+
+else:
+    print("Failed to retrieve the page")
