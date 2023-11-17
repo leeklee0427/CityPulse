6f14ea3c85ba016e5dfa6410202b92a760029fbb
leeklee0427 <billlee0427@126.com>
2023-11-17 01:41:13 -0500

    Updated crawlers and data

--- crawlers/employment_rate.py
+++ crawlers/employment_rate.py
@@ -2,7 +2,10 @@
 File: employment_rate.py
 Author: Louie SUN (hongyuns), Daochen LI (daochenl)
 Description: Retrieve city and employment rate data from Census using API
-Website: https://data.census.gov
+
+Website:
+https://data.census.gov
+https://www.census.gov/library/reference/code-lists/ansi.html#place
 """
 
 import json
@@ -11,6 +14,16 @@ import requests
 
 url_raw = ("https://api.census.gov/data/2022/acs/acs1/profile?get=NAME,"
            "DP03_0004PE&for=place:*&in=state:*&key=f8579687edb9f7e019a2b54348ff323abd3a0a53")
+
+def format_specific_cities(df):
+    city_replacements= {
+        'Indianapolis (balance)': 'Indianapolis', 'Urban Honolulu CDP': 'Honolulu', 'Boise City': 'Boise',
+        'St. Louis': 'Saint Louis'
+    }
+    for original, new_format in city_replacements.items():
+        df['City'] = df['City'].str.replace(original, new_format, regex=False)
+
+    return df
 
 def split_name(name):
     city, state_name = name.split(", ")
@@ -65,50 +78,57 @@ if __name__ == "__main__":
     else:
         print("Error fetching data:", response_raw.status_code)
 
-    mem_data = fetch_data("48000", "47")
-    ba_data = fetch_data("4000", "24")
-    de_data = fetch_data("22000", "26")
-    al_data = fetch_data("02000", "35")
-    sa_data = fetch_data("65000", "29")
-    oa_data = fetch_data("53000", "06")
-    mi_data = fetch_data("53000", "55")
-    new_data = fetch_data("55000", "22")
-    chi_data = fetch_data("14000", "17")
-    phi_data = fetch_data("60000", "42")
-    atl_data = fetch_data("04000", "13")
-    hou_data = fetch_data("35000", "48")
-    san_data = fetch_data("67000", "06")
-    was_data = fetch_data("50000", "11")
-    ind_data = fetch_data("36003", "18")
-    por_data = fetch_data("60545", "23")
-    las_data = fetch_data("40000", "32")
-    min_data = fetch_data("43000", "27")
-    sea_data = fetch_data("63000", "53")
-    tuc_data = fetch_data("77000", "04")
-    pho_data = fetch_data("55000", "04")
-    mia_data = fetch_data("45000", "12")
-    los_data = fetch_data("44000", "06")
-    dal_data = fetch_data("19000", "48")
-    col_data = fetch_data("19000", "13")
-    ny_data = fetch_data("51000", "36")
-    sac_data = fetch_data("64000", "06")
-    orl_data = fetch_data("53000", "12")
-    sana_data = fetch_data("65000", "48")
-    hon_data = fetch_data("71550", "15")
-    den_data = fetch_data("20000", "08")
-    tam_data = fetch_data("71000", "12")
-    aus_data = fetch_data("05000", "48")
-    sand_data = fetch_data("66000", "06")
-    bos_data = fetch_data("07000", "25")
-    boi_data = fetch_data("08830", "16")
-    sal_data = fetch_data("67000", "49")
+    locations = {
+        "mem": ("48000", "47"),
+        "ba": ("4000", "24"),
+        "de": ("22000", "26"),
+        "al": ("02000", "35"),
+        "sa": ("65000", "29"),
+        "oa": ("53000", "06"),
+        "mi": ("53000", "55"),
+        "new": ("55000", "22"),
+        "chi": ("14000", "17"),
+        "phi": ("60000", "42"),
+        "atl": ("04000", "13"),
+        "hou": ("35000", "48"),
+        "san": ("67000", "06"),
+        "was": ("50000", "11"),
+        "ind": ("36003", "18"),
+        "por": ("60545", "23"),
+        "las": ("40000", "32"),
+        "min": ("43000", "27"),
+        "sea": ("63000", "53"),
+        "tuc": ("77000", "04"),
+        "pho": ("55000", "04"),
+        "mia": ("45000", "12"),
+        "los": ("44000", "06"),
+        "dal": ("19000", "48"),
+        "col": ("19000", "13"),
+        "ny": ("51000", "36"),
+        "sac": ("64000", "06"),
+        "orl": ("53000", "12"),
+        "sana": ("65000", "48"),
+        "hon": ("71550", "15"),
+        "den": ("20000", "08"),
+        "tam": ("71000", "12"),
+        "aus": ("05000", "48"),
+        "sand": ("66000", "06"),
+        "bos": ("07000", "25"),
+        "boi": ("08830", "16"),
+        "sal": ("67000", "49"),
+        "charlotte": ("12000", "37"),
+        "raleigh": ("55000", "37"),
+        "columbus": ("18000", "39"),
+        "portland": ("59000", "41"),
+        "kansas": ("38000", "29"),
+        "pittsburgh": ("61000", "42"),
+        "sanjose": ("68000", "06"),
+        "madison": ("48000", "55")
+    }
 
-    combined_data = pd.concat([mem_data, ba_data, de_data, al_data, sa_data, oa_data, mi_data, new_data, chi_data,
-                            phi_data, atl_data, hou_data, san_data, was_data, ind_data, por_data,
-                            las_data, min_data, sea_data, tuc_data, pho_data, mia_data, los_data,
-                            dal_data, col_data, ny_data, sac_data, orl_data, sana_data, hon_data,
-                            den_data, tam_data, aus_data, sand_data, bos_data, boi_data, sal_data], ignore_index=True)
-
+    combined_data = pd.concat([fetch_data(code[0], code[1]) for code in locations.values()], ignore_index=True)
+    combined_data = format_specific_cities(combined_data)
+    
     filename = './data/clean/employment_rate.csv'
     combined_data.to_csv(filename, index=False)
 
