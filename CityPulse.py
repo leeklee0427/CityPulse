07476145f0a5356b1ae6d08bd7a87168fe9fadcc
leeklee0427 <billlee0427@126.com>
2023-12-02 04:56:03 -0500

    Added CityPulse.py

---
+++ CityPulse.py
@@ -0,0 +1,30 @@
+"""
+File: CityPulse.py
+Author: Daochen LI (daochenl)
+Description: CityPulse complete application
+"""
+
+from crawlers import average_household_income, cost_of_living, employment_rate, health_care_index, pollution_index, recreation, safety_index
+from data import data_processing
+from gui import ui
+
+
+if __name__ == "__main__":
+    
+    print("Citypulse complete application")
+    
+    # Obtaining data
+    average_household_income.fetch_income()
+    employment_rate.fetch_employment()
+    cost_of_living.crawl_cost()
+    recreation.crawl_recreation()
+    health_care_index.crawl_health()
+    pollution_index.crawl_pollution()
+    safety_index.crawl_safety_index()
+
+    # Processing data
+    data_processing.process_data()
+
+    # User interface
+    ui.launch()
+
